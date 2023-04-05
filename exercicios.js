7666efe38456e1f4fa21cbc7b1424c6742ff80d2
Márcio <marciotahyra@gmail.com>
2023-04-05 01:49:22 -0300

    terceiro commit

--- exercicios.js
+++ exercicios.js
@@ -13,3 +13,10 @@ document.write("b) ", b2 ,typeof(b2), " -> ", parseFloat(b2), typeof(parseFloat(
 document.write("c) ", c2 ,typeof(c2), " -> ", parseFloat(c2), typeof(parseFloat(c2)), "<br>")
 document.write("d) ", d2 ,typeof(d2), " -> ", parseFloat(d2), typeof(parseFloat(d2)), "<br>")
 document.write("<br>")
+
+document.write("3) <br>")
+let a3 = "A MAIOR TECNOLOGIA", b3 = "HARDWARE", c3 = "FIAP A MELHOR FACULDADE", d3 = "TENCOLOGIA"
+document.write("a) ", a3, ": ", a3.length, "<br>")
+document.write("b) ", b3, ": ", b3.length, "<br>")
+document.write("c) ", c3, ": ", c3.length, "<br>")
+document.write("d) ", d3, ": ", d3.length, "<br>")
