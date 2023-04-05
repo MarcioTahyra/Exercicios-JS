ae756dd75d332f5bd84635f44c22b979f9614f9e
Márcio <marciotahyra@gmail.com>
2023-04-05 01:31:30 -0300

    primeiro commit

---
+++ exercicios.js
@@ -0,0 +1,5 @@
+let a = 30.345, b = 154.666, c = 50.888, d = 089.52
+document.write("a) ", a ," -> ", parseInt(a) , "<br>")
+document.write("b) ", b ," -> ", parseInt(b) , "<br>")
+document.write("c) ", c ," -> ", parseInt(c) , "<br>")
+document.write("d) ", d ," -> ", parseInt(d) , "<br>")
