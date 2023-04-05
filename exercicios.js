4ebe1d3f5b57f52531d5074b79655d0462eaa87d
Márcio <marciotahyra@gmail.com>
2023-04-05 01:43:12 -0300

    segundo commit

--- exercicios.js
+++ exercicios.js
@@ -1,5 +1,15 @@
+document.write("1) <br>")
 let a = 30.345, b = 154.666, c = 50.888, d = 089.52
 document.write("a) ", a ," -> ", parseInt(a) , "<br>")
 document.write("b) ", b ," -> ", parseInt(b) , "<br>")
 document.write("c) ", c ," -> ", parseInt(c) , "<br>")
 document.write("d) ", d ," -> ", parseInt(d) , "<br>")
+document.write("<br>")
+
+document.write("2) <br>")
+let a2 = "30.345", b2 = "154.666", c2 = "50.888", d2 = "089.52"
+document.write("a) ", a2 ,typeof(a2), " -> ", parseFloat(a2), typeof(parseFloat(a2)), "<br>")
+document.write("b) ", b2 ,typeof(b2), " -> ", parseFloat(b2), typeof(parseFloat(b2)), "<br>")
+document.write("c) ", c2 ,typeof(c2), " -> ", parseFloat(c2), typeof(parseFloat(c2)), "<br>")
+document.write("d) ", d2 ,typeof(d2), " -> ", parseFloat(d2), typeof(parseFloat(d2)), "<br>")
+document.write("<br>")
